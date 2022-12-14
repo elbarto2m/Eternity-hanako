5fe4009759e74ed598135b174535abb306bac224
Elbarto <113073680+elbarto2m@users.noreply.github.com>
2022-12-14 16:47:16 +0100

    Update mods.js

--- Organs/commands/General/mods.js
+++ Organs/commands/General/mods.js
@@ -11,7 +11,7 @@ module.exports={
     start:async(client,m,{})=>{
         const mod= owner
         let mo=`
-*🎉 Nezuko Mods 🎉*\n`
+*🎉 Hanako Mods 🎉*\n`
 for(let i=0;i<mod.length;i++){
     const um= await client.username(mod[i]+'@s.whatsapp.net')
     mo+=`\n✨${i+1}\n*👤 Name:* ${um}\n*📱 Contact:* http://wa.me/+${mod[i].split("@")[0]}\n`
